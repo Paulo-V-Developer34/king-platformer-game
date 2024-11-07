883482045c94580e6ce1d2b0bab73bbfec741ebc
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2024-11-07 15:12:36 -0300

    player as an class

--- index.js
+++ index.js
@@ -5,23 +5,45 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16 //considerando que nosso jogo será baseado em blocos 64x64 podemos considerar que podemos colocar 16 blocos de largura
 canvas.height = 64 * 9
 
-//config do personagem
-let y = 100
-const pheight = 100 //100 é a altura do personagem
-let pbotton = y + pheight
+class Player {
+    constructor () {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.looklike = {
+            width: 100,
+            height: 100,
+            color: "red"
+        }
+        this.sides = {
+            bottom: this.position.y + this.looklike.height
+        }
+    }
+
+    draw() {
+        c.fillStyle = this.looklike.color
+        c.fillRect(this.position.x, this.position.y, this.looklike.width, this.looklike.height)
+    }
+    
+    update() {
+        if(this.sides.bottom < canvas.height) {
+            this.position.y++
+            this.sides.bottom = this.position.y + this.looklike.height
+        }
+    }
+}
+
+const player = new Player()
+
 function animate() {
     window.requestAnimationFrame(animate)
     console.log("go")
     c.fillStyle = 'white' //esses comandos irão criar algum elemento no canvas
     c.fillRect(0, 0, canvas.width, canvas.height) //posição do elemento
 
-    c.fillStyle = 'red'
-    c.fillRect(100, y, 100, pheight)
-
-    if(pbotton < canvas.height) {
-        y++
-        pbotton = y + pheight //antes um erro estava acontecendo, pois eu não estava atualizando esse valor, porque achei que isso seria feito altomaticamente, quando na verdade não é
-    }
+    player.draw()
+    player.update()
 }
 
 animate()
